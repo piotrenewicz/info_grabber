51a90d337fcf73c5c770783693143b37fbb2fb1a
Konrad Maciejczyk <conradmac97@gmail.com>
2021-04-28 22:07:59 +0200

    first version of UI

--- app/src/main/java/com/example/infograbber/MainActivity.kt
+++ app/src/main/java/com/example/infograbber/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.infograbber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var WebsiteAdapter: WebsiteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WebsiteAdapter = WebsiteAdapter(mutableListOf())
+        AppWebsiteList.adapter = WebsiteAdapter
+        AppWebsiteList.layoutManager = LinearLayoutManager(this)
+
+        AppWebsiteSeconds.addTextChangedListener {
+            if (it.toString().length == 1 && it.toString().startsWith("0")) {
+                it!!.clear()
+            }
+        }
+
+        AppDeleteButton.setOnClickListener {
+            WebsiteAdapter.deleteWebsite()
+        }
+
+        AppAddButton.setOnClickListener {
+            val websiteURL: String = AppWebsiteURL.text.toString()
+            val websiteTimeInterval: String = AppWebsiteSeconds.text.toString()
+            val websiteTitle: String = AppWebsiteTitle.text.toString()
+
+            val website: Website = Website(this@MainActivity, websiteTitle, websiteURL, websiteTimeInterval.toInt())
+            WebsiteAdapter.addWebsite(website)
+
+            //Clearing out input fields
+            AppWebsiteSeconds.text.clear()
+            AppWebsiteURL.text.clear()
+            AppWebsiteTitle.text.clear()
+        }
     }
 }
---
+++ app/src/main/java/com/example/infograbber/Website.kt
@@ -0,0 +1,6 @@
+package com.example.infograbber
+
+import android.content.Context
+
+class Website(context: Context, val title: String, val URL:String, val timeInterval: Int, val domain: String = "example.com", var isChecked: Boolean = false) {
+}
