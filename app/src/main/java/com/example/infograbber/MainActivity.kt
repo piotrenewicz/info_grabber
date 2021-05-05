e47450d5b3f40708a6e4cdc6961b655503a3210d
Konrad Maciejczyk <conradmac97@gmail.com>
2021-05-05 20:54:04 +0200

    added coroutine to fetch website source

--- app/src/main/java/com/example/infograbber/MainActivity.kt
+++ app/src/main/java/com/example/infograbber/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.example.infograbber
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     private lateinit var WebsiteAdapter: WebsiteAdapter
+
+    private suspend fun getWebsiteSource(context: Context, URL: String): Document? = try {
+        val doc: Document = Jsoup.connect(URL).get()
+        println(doc.title())
+        doc
+    }catch(e: Exception){
+        withContext(Dispatchers.Main){
+            Toast.makeText(context, "Error fetching URL", Toast.LENGTH_LONG).show()
+        }
+        null
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +60,17 @@ class MainActivity : AppCompatActivity() {
             AppWebsiteSeconds.text.clear()
             AppWebsiteURL.text.clear()
             AppWebsiteTitle.text.clear()
+
+            CoroutineScope(IO).launch(){
+                val websiteSource: Document? = getWebsiteSource(this@MainActivity, websiteURL)
+                var result: String= ""
+                if (websiteSource == null){
+                    result = "Error while fetching website"
+                }else{
+                    result = websiteSource.html()
+                }
+                println("Website: $result")
+            }
         }
     }
 }
