b062a46159ca7e063eb4155835b085ad6c16dd61
Konrad Maciejczyk <conradmac97@gmail.com>
2021-05-19 20:40:56 +0200

    added example website item.

--- app/src/main/java/com/example/infograbber/AddWebsiteActivity.kt
+++ app/src/main/java/com/example/infograbber/AddWebsiteActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
-import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_add_website.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
--- app/src/main/java/com/example/infograbber/MainActivity.kt
+++ app/src/main/java/com/example/infograbber/MainActivity.kt
@@ -2,17 +2,32 @@ package com.example.infograbber
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var WebsiteAdapter: WebsiteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        WebsiteAdapter = WebsiteAdapter(mutableListOf())
+        AppWebsiteList.adapter = WebsiteAdapter
+        AppWebsiteList.layoutManager = LinearLayoutManager(this)
 
         AppAddButton.setOnClickListener{
             val intent = Intent(this, AddWebsiteActivity::class.java)
             startActivity(intent)
         }
+
+        //Website item example
+        val websiteTitle: String = "Onet.pl"
+        val websiteURL: String = "https://www.onet.pl"
+        val websiteTimeInterval: Int = 30
+
+        val website: Website = Website(websiteTitle, websiteURL, websiteTimeInterval)
+        //Adding example website to recyclerView Adapter
+        WebsiteAdapter.addWebsite(website)
 
     }
 }
