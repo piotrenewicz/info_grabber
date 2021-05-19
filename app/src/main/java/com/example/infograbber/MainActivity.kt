22617c5501ceabb6c6f5e1ae666a12e02e7e467b
Mykhailo Marchenko <mykm@opinov8.com>
2021-05-19 21:02:44 +0200

    Instantly add new website to main screen

--- app/src/main/java/com/example/infograbber/MainActivity.kt
+++ app/src/main/java/com/example/infograbber/MainActivity.kt
@@ -20,25 +20,14 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        //Website item example
-        val websiteTitle: String = "Onet.pl"
-        val websiteURL: String = "https://www.onet.pl"
-        val websiteTimeInterval: Int = 30
+    }
 
-        val website: Website = Website(websiteTitle, websiteURL, websiteTimeInterval)
-        //Adding example website to recyclerView Adapter
-        WebsiteAdapter.addWebsite(website)
-
-        // START try json read
-        // No sites no data – add one via application and restart it, it works
-        // How to refresh this list when we enter into this activity, not only on creation?
+    override fun onStart() {
+        super.onStart()
         val websites = readWebsiteList(applicationContext)
         for (site in websites)
         {
-            println("WEBSITE $site")
             WebsiteAdapter.addWebsite(site)
         }
-        // END try json read
-
     }
 }
