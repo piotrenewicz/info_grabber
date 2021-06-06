372ef33edb406dcc76c0649073391c7f8a358c09
Piotrenewicz <piotrekm9@onet.eu>
2021-06-07 01:08:15 +0200

    removed timeInterval, dropping auto refreshing feature

--- app/src/main/java/com/example/infograbber/infoElement.kt
+++ app/src/main/java/com/example/infograbber/infoElement.kt
@@ -6,7 +6,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Website(val title: String,
                    val URL:String,
-                   val timeInterval: Int,
                    val awlCommand: String = "",
                    val domain: String = "https://www.example.com",
                    var isChecked: Boolean = false)
