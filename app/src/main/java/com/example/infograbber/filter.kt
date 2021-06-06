d55604710992c994fda50f374b837c3c608e60fb
Piotrenewicz <piotrekm9@onet.eu>
2021-06-07 01:20:39 +0200

    cleaned up my code, in filter.kt, and gave it better names

--- app/src/main/java/com/example/infograbber/filter.kt
+++ app/src/main/java/com/example/infograbber/filter.kt
@@ -2,47 +2,36 @@
 
 package com.example.infograbber
 import android.content.Context
-import kotlinx.android.synthetic.main.activity_add_website.*
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.lang.Runtime
 
 fun syscall(commmand: String): String {
-    var out: String
+    var output: String
     try{
         val script: Array<String> = arrayOf("/system/bin/sh", "-c", commmand)
         val process: Process = Runtime.getRuntime().exec(script)
-        val std: BufferedReader = BufferedReader(InputStreamReader(process.inputStream))
+        val stdout: BufferedReader = BufferedReader(InputStreamReader(process.inputStream))
         process.waitFor()
-        out = std.readText()
-        println(""" 
-            !Command run!""".trimIndent())
-        println(commmand)
-        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        println(out)
-        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+        output = stdout.readText()
+//        println("""
+//            !Command run!""".trimIndent())
+//        println(commmand)
+//        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+//        println(out)
+//        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     }catch (e: IOException){
         e.printStackTrace();
-        out = e.toString()
+        output = e.toString()
     }
-    return out
+    return output
 }
 
 
-fun html_inserter(c: Context, html: String, command: String, callback: (output: String) -> Unit){
+fun syscall_with_html(c: Context, html: String, command: String, callback: (output: String) -> Unit){
     ramfs(c, "html_", html){ rampath ->
         val rc: String = "alias html=\"cat $rampath\"\n$command\n"
         callback(syscall(rc))
     }
-//    val rc: String = """
-//alias html='echo \'""" + html + """\''
-//""" + command
-
-}
-
-fun filter_testing(){
-    println("pretest")
-    println(syscall("echo lol"))
-    println("posttest")
 }
